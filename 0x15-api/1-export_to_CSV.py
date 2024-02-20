58474885d1eb2466a834cd30e161b48f69d4a249
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-20 08:13:55 +0100

    csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,12 +8,12 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    userId = sys.argv[1]
-    user = requests.get(url + "users/{}".format(userId)).json()
-    todos = requests.get(url + "todos", params={"userId": userId}).json()
+    userID = sys.argv[1]
+    user = requests.get(url + "users/{}".format(userID)).json()
+    todos = requests.get(url + "todos", params={"userId": userID}).json()
     username = user.get("username")
 
-    with open("{}.csv".format(userId), 'w', newline="") as file:
+    with open("{}.csv".format(userID), 'w', newline="") as file:
         line = csv.writer(file, quoting=csv.QUOTE_ALL)
         [line.writerow(
             [userId, username, todo.get("completed"), todo.get("title")]
