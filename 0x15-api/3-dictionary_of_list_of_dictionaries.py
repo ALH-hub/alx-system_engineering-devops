e369f5dda7b426fec22ca23cdf8a71ae7d071e64
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-20 09:46:00 +0100

    json object of objects"

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,13 +9,12 @@ if __name__ == "__main__":
     user_url = "https://jsonplaceholder.typicode.com/users"
     todo_url = "https://jsonplaceholder.typicode.com/todos"
     users = requests.get(user_url).json()
-    todos = requests.get(todo_url).json()
 
     with open("todo_all_employees.json", 'w') as file:
-        for user in users:
-            todos_user = requests.get(todo_url, params={"userId": user.get("id")}).json()
-            json.dump({user.get("id"): [{
-                "task": todo.get("title"),
-                "completed": todo.get("completed"),
-                "username": user.get("username")
-                } for todo in todos_user]}, file)
+        json.dump({user.get("id"): [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user.get("username")
+            } for todo in requests.get(todo_url,
+                                       params={"userId": user.get("id")}).json()]
+                   for user in users}, file)
