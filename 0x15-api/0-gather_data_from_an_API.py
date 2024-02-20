a67e1ecea488632cf6e4c8d4b9ec4014b0d46122
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-20 07:28:11 +0100

    gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""gather data from an API"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [todo.get("title") for todo in todos
+                 if todo.get("completed") is True]
+    print("Employee {} is done with task({}/{})"
+          .format(user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(task)) for task in completed]
