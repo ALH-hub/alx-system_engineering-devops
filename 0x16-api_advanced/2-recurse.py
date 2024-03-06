6e54d4ab5e91735f3bd23d84b7c1fd0735c021fc
Alhadji Oumate <alhadjioum0@gmail.com>
2024-03-06 02:09:47 +0100

    api recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def top_ten(subreddit):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """Qureries the Reddit API for the number of subredit.
     Args:
         subreddit: the name of the subreddit
@@ -13,15 +13,26 @@ def top_ten(subreddit):
     headers = {"User-Agent": "0x16.api.advanced/v1.0"}
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     params = {
-        "limit": 10
+        "after": after,
+        "count": count,
+        "limit": 100
     }
 
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
 
     if response.status_code == 404:
-        print("None")
-        return
+        return None
 
     data = response.json().get("data")
-    [print(val.get("data").get("title")) for val in data.get("children")]
+    after = data.get("after")
+    count += data.get("dist")
+    for i in data.get("children"):
+        hot_list.append(i.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+
+    print(hot_list)
+
+    return hot_list
