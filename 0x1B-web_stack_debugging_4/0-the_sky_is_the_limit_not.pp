1d0ee6d06ecbaf858d2493380f18f59d4add0d93
Alhadji Oumate <alhadjioum0@gmail.com>
2024-03-15 02:35:47 +0100

    puppet fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,10 @@
-# Increases the amount of traffic an Nginx server can handle.
+#increase the amount of traffic to handle
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
+exec { 'nginx-fix':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart Nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
