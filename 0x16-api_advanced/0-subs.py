cb950fd970283db9d54719100dc34e16ab434f22
Alhadji Oumate <alhadjioum0@gmail.com>
2024-04-12 00:41:06 +0100

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""advanced apis"""
+"""query subscribers on a given Reddit subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Qureries the Reddit API for the number of subredit.
-    Args:
-        subreddit: the name of the subreddit
-    Returns:
-        The number of subscribers(integer) or 0 if the subreddit is invalid
     """
-    headers = {"User-Agent": "0x16.api.advanced/v1.0"}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-    except requests.exceptions.RequestException:
-        return 0
-
+    return number of subscribers on a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-
-    data = response.json().get("data")
-    return data.get("subscribers", 0)
+    results = response.json().get("data")
+    return results.get("subscribers")
