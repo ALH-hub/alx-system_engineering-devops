34ca431961d5ff78a2c701f2306f760e56ddbcf4
Alhadji Oumate <alhadjioum0@gmail.com>
2024-04-12 01:30:14 +0100

    recurse it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,33 +4,27 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Qureries the Reddit API for the number of subredit.
-    Args:
-        subreddit: the name of the subreddit
-    Returns:
-        prints the top ten
-    """
-    headers = {"User-Agent": "0x16.api.advanced/v1.0"}
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    """Returns a list of all hot posts"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "MyRedditSubscriberCounter Version 1.0.0"
+    }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
     if response.status_code == 404:
         return None
 
-    data = response.json().get("data")
-    after = data.get("after")
-    count += data.get("dist")
-    for i in data.get("children"):
-        hot_list.append(i.get("data").get("title"))
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
-
     return hot_list
