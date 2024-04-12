6236ae5aef6157b2fd5d990c08836f04314f1576
Alhadji Oumate <alhadjioum0@gmail.com>
2024-04-12 01:05:21 +0100

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""query subscribers on a given Reddit subreddit"""
+"""
+Retrieves the number of subscribers for a given Reddit subreddit.
+
+This script utilizes the Requests library to make an HTTP GET request to the
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    return number of subscribers on a given subreddit
+    Fetches the number of subscribers for a Reddit subreddit.
+    Args:
+        subreddit (str): The name of the subreddit.
+    Returns:
+        int: The number of subscribers for the subreddit, or 0 if not found.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+      "User-Agent": "MyRedditSubscriberCounter/v1.0 (by u/Complete-Visual894)"
+      }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
